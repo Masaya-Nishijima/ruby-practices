a449667865fce1c8b2d82683a8ebf92a9ee08c99
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-03-14 17:29:36 +0900

    引数がない場合に今日の日付を入れるように
    引数で指定がない月or年をDateクラスのTodayメソッドから取得するように記述した。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,10 +1,15 @@
 params = {}
 
+require 'date'
+params[:month] = Date.today.month
+params[:year] = Date.today.year
+
 require 'optparse'
 opt = OptionParser.new
 opt.on('-m VAL', '--month')
 opt.on('-y VAL', '--year')
 opt.parse!(ARGV, into: params)
+
 
 
 puts "      #{params[:month]}月 #{params[:year]}"
