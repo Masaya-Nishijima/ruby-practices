54b2ac7d172cadb483e9f3d931a618d9327d23ef
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-03-16 09:03:15 +0900

    今までの記述にコメントを追記
    コードに一切コメントを付けていなかったので、今まで記述したコードの機能をコメントとして追記した。
    今後は、コードの記述と平行してコメントを付ける。

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,10 +1,10 @@
 params = {}
 
-require 'date'
+require 'date'  #現在の日付を取得する。
 params[:month] = Date.today.month
 params[:year] = Date.today.year
 
-require 'optparse'
+require 'optparse'  #コマンドライン引数から月・年を取る
 opt = OptionParser.new
 opt.on('-m VAL', '--month')
 opt.on('-y VAL', '--year')
@@ -12,4 +12,4 @@ opt.parse!(ARGV, into: params)
 
 
 
-puts "      #{params[:month]}月 #{params[:year]}"
+puts "      #{params[:month]}月 #{params[:year]}" #一行目の表示用
