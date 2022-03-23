940ee641615333dee26795976cd13e92c293e7bb
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-03-23 09:13:20 +0900

    リファクタリング
    条件判断の関数と、判断後の処理両方で条件分岐をしていた点を修正

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,26 +1,17 @@
 def pattern_selecter(num)
-  multiple = 1
   if ((num % 3) == 0) && ((num % 5) ==0)
-    multiple = 15
+    "FizzBuzz"
   elsif (num % 3) == 0
-    multiple = 3
+    "Fizz"
   elsif (num % 5) == 0
-    multiple = 5
+    "Buzz"
+  else
+    num
   end
-  return multiple
 end
 
 
 20.times do |num|
   num += 1
-  case pattern_selecter(num)
-  when 15
-    puts "FizzBuzz"
-  when 3
-    puts "Fizz"
-  when 5
-    puts "Buzz"
-  else
-    puts num
-  end
+  puts pattern_selecter(num)
 end
