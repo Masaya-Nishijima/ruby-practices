57182c44d07bdb5892d25ad63742696fd3d5d849
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-04-06 14:51:28 +0900

    長いファイル名への表示の対応
    表示を24字幅固定から、最低24字幅、名前の長いファイルがあるときは7字空白をと修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -32,10 +32,12 @@ files = # カレントディレクトリor指定パスのファイルを取得�
     list_up_files(File.absolute_path(ARGV[0]))
   end
 
+display_width = files.map(&:length).max +  7 # 長い名前のファイルがあって、最低でも7マスは空白ができるように設定
+if display_width < 24 then display_width = 24 end # デフォルトのファイル名の幅 組み込みlsを参考に設定
 files = sort_array(files.sort)
 files.size.times do |time|
   WIDTH.times do |column|
-    printf('%-24s', files[time][column])
+    printf('%-*s', display_width, files[time][column])
   end
   puts
 end
