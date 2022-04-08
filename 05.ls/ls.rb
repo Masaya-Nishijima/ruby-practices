bf511850cd9b342611b72e043b7756ea890afc7c
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-04-08 17:19:52 +0900

    ALLオプションを追加 -a

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+
+params = {}
+opt = OptionParser.new
+opt.on('-a', '--all') { |v| params[:all] = v }
+opt.parse!(ARGV)
+
 WIDTH = 3
 
 # 入力された一次元配列を、WIDTH幅の二次元配列にする関数
@@ -8,7 +15,7 @@ def sort_array(array)
   height = (array.size.to_f / WIDTH).ceil
   answer_array = []
 
-  if array.size < 3 # 要素数が3以下のときの処理(一行で表示が終わる)
+  if array.size < WIDTH # 要素数が3以下のときの処理(一行で表示が終わる)
     middle_array = Array.new(array.size) { |time| array[time] }
     return answer_array << middle_array
   end
@@ -21,10 +28,17 @@ def sort_array(array)
 end
 
 files = # カレントディレクトリor指定パスのファイルを取得する
-  if ARGV[0].nil?
-    Dir.glob('*', base: Dir.getwd)
+  if params[:all].nil?
+    if ARGV[0].nil?
+      Dir.glob('*', base: Dir.getwd)
+    else
+      Dir.glob('*', base: File.absolute_path(ARGV[0]))
+    end
+  elsif ARGV[0].nil?
+    Dir.glob('*', File::FNM_DOTMATCH, base: Dir.getwd)
   else
-    Dir.glob('*', base: File.absolute_path(ARGV[0]))
+    Dir.glob('*', File::FNM_DOTMATCH, base: File.absolute_path(ARGV[0]))
+
   end
 
 display_width = [files.map(&:length).max + 7, 24].max # 最低でも7マスは空白ができるように設定 デフォルトのファイル名の幅として24を指定している。 組み込みlsを参考に設定
