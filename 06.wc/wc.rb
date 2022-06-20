265c1560a96586b7ffa8c56e242014f5b2c5c43d
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-20 11:58:58 +0900

    feat: 複数のファイルを指定されたときに、totalを表示するように調整

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,16 +6,15 @@ require 'etc'
 TEMP_FILE = '.tmp_wc'
 WIDTH = 5
 
-def main(params)
-  has_path = ARGV[0].nil? ? TEMP_FILE : File.absolute_path(ARGV[0])
-  if has_path == TEMP_FILE
-    File.open(has_path, 'w+') do |tmp_file|
+def main(params, path)
+  if path == TEMP_FILE
+    File.open(path, 'w+') do |tmp_file|
       readlines.each { |line| tmp_file.write line }
     end
   end
 
-  File.open(has_path) do |file|
-    file_name = has_path == TEMP_FILE ? '' : has_path.split('/')[-1]
+  File.open(path) do |file|
+    file_name = path == TEMP_FILE ? '' : path.split('/')[-1]
     file_body = file.read
     file_size = file.size
     n_lines = file_body.count("\n")
@@ -27,6 +26,7 @@ def main(params)
       printf("%#{WIDTH}d %#{WIDTH}d ", n_words, file_size, file_name)
     end
     printf("%s\n",file_name)
+    [n_lines, n_words, file_size]
   end
 end
 
@@ -41,6 +41,11 @@ end
 
 params = read_option
 
-main params
+has_path = ARGV[0].nil? ? [TEMP_FILE] : ARGV.each { |file| File.absolute_path(file) }
 
-exec("ruby #{__dir__}/wc.rb#{' -l' if params[:lines]} #{ARGV.drop(1).join(' ')}") unless ARGV[1].nil?
+total = []
+has_path.each { |file|  total << main(params, file) }
+unless has_path[1].nil?
+  total.transpose.map(&:sum).each { |value| printf("%#{WIDTH}d ", value)}
+  puts("total")
+end
