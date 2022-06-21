955601943cc5a285dd8e004b62e26fbad5e483af
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-21 10:33:06 +0900

    style: 全体の記述順序等を修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,6 +6,17 @@ require 'etc'
 TEMP_FILE = '.tmp_wc'
 WIDTH = 5
 
+def main
+  params = read_option
+  files = ARGV[0].nil? ? [{ name: '', body: readlines.join }] : ARGV.map { |file| { name: file, body: File.open(file).read } }
+  total = files.map { |file| count_and_print_wc_element(params, file) }
+  if files.size >= 2
+    total.transpose.map(&:sum).each { |value| printf("%#{WIDTH}d ", value) }
+    puts('total')
+  end
+  total # rubocop Style/GuardClause: Use a guard claus対策
+end
+
 def count_and_print_wc_element(params, file)
   file_size = file[:body].bytesize
   n_lines = file[:body].count("\n")
@@ -26,11 +37,4 @@ def read_option
   params
 end
 
-params = read_option
-
-has_path = ARGV[0].nil? ? [{ name: '', body: readlines.join }] : ARGV.map { |file| { name: file, body: File.open(file).read } }
-total = has_path.map { |file| count_and_print_wc_element(params, file) }
-unless has_path[1].nil?
-  total.transpose.map(&:sum).each { |value| printf("%#{WIDTH}d ", value) }
-  puts('total')
-end
+main
