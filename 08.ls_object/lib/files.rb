cadd744439d81f4a41577b7e313601ade684f817
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-21 16:21:23 +0900

    feat: Filesクラスのprint_long_formatメソッド作成

--- 08.ls_object/lib/files.rb
+++ 08.ls_object/lib/files.rb
@@ -1,9 +1,13 @@
+require 'etc'
+
 class Files # 疑問 クラス名に複数形を使ってよいか?ファイル群 Files.new().reverseのような使い方を想定
   WIDTH = 3
+  TYPE_HASH = { '01' => 'p', '02' => 'c', '04' => 'd', '06' => 'b', '10' => '-', '12' => 'l', '14' => 's' }.freeze
+  PERMITS = ['---', '--x', '-w-', '-wx', 'r--', 'r-x', 'r-w', 'rwx'].freeze
   def initialize(path = nil, all = nil)
     has_all = all.nil? ? 0 : File::FNM_DOTMATCH
-    has_path = path.nil? ? Dir.getwd : File.absolute_path(path)
-    @files_names = Dir.glob('*', has_all, base: has_path).sort # 疑問 (複数あるファイル)の名前 = files_names
+    @has_path = path.nil? ? Dir.getwd : File.absolute_path(path)
+    @files_names = Dir.glob('*', has_all, base: @has_path).sort # 疑問 (複数あるファイル)の名前 = files_names
     if @files_names == []
       @files_names[0] = path
       has_path.sub!(/[.a-zA-Z0-9]+$/, '')
@@ -23,6 +27,71 @@ class Files # 疑問 クラス名に複数形を使ってよいか?ファイル�
 
   def reverse!
     @files_names.reverse!
+  end
+
+  def print_long_format()
+    files = @files_names.map { |file| { name: file, info: File.lstat("#{@has_path}/#{file}") } }
+    files.map! do |file|
+      {
+        mode: file[:info].mode,
+        link: file[:info].nlink,
+        owner: Etc.getpwuid(file[:info].uid).name,
+        group: Etc.getgrgid(file[:info].gid).name,
+        size: file[:info].size,
+        time: file[:info].mtime,
+        name: file[:name]
+      }
+    end
+    widthes = select_widthes(files)
+    printf("total\s%d\n", (files.map { |file| file[:size] }.max / 512.to_f).ceil)
+    files.each do |file|
+      print_line(file, widthes)
+    end
+  end
+
+  def select_widthes(files)
+    widthes = {}
+    widthes[:link_width] = files.map { |file| file[:link] }.max.to_s.length
+    widthes[:owner_width] = files.map { |file| file[:owner].length }.max
+    widthes[:group_width] = files.map { |file| file[:group].length }.max
+    widthes[:size_width] = files.map { |file| file[:size] }.max.to_s.length
+    widthes[:time_width] = 2
+    widthes
+  end
+
+  def print_line(file, widthes)
+    print_type_and_parmit(file[:mode])
+    printf("%#{widthes[:link_width]}d\s", file[:link])
+    printf("%#{widthes[:owner_width]}s\s\s", file[:owner])
+    printf("%#{widthes[:group_width]}s\s\s", file[:group])
+    printf("%#{widthes[:size_width]}d\s", file[:size])
+    printf("%#{widthes[:time_width]}d\s%#{widthes[:time_width]}d\s", file[:time].month, file[:time].day)
+    printf("%#{widthes[:time_width]}d:%#{widthes[:time_width]}d\s", file[:time].hour, file[:time].min)
+    puts file[:name]
+  end
+
+  def print_type_and_parmit(file_mode)
+    # ファイルタイプとパーミッションを八進数7桁文字列に変換
+    type_and_permisson = format('%#07o', file_mode)
+
+    # ファイルタイプの記述
+    type = type_and_permisson.slice(1, 2)
+    print_type(type)
+
+    # 各対象ごとにパーミッションを記述
+    3.times do |time|
+      permisson = type_and_permisson[time + 4]
+      print_permisson(permisson)
+    end
+    printf "\s\s"
+  end
+
+  def print_type(type)
+    print TYPE_HASH[type]
+  end
+
+  def print_permisson(permisson)
+    print PERMITS[permisson.to_i]
   end
 
   private
--- 08.ls_object/spec/files_spec.rb
+++ 08.ls_object/spec/files_spec.rb
@@ -23,12 +23,22 @@ RSpec.describe 'ファイル群' do
         files.print_short_format
       end
     end
+
     context 'ファイル群の反転表示' do
       it '指定ディレクトリの反転表示' do
         files = Files.new Dir.getwd + '/spec/test_dir'
         files.reverse!
         expect { files.print_short_format }.to output(/test_file9\s*test_file4/).to_stdout
         files.print_short_format
+      end
+    end
+
+    context "ファイル群をロング表示" do
+      it '指定ディレクトリをロング表示' do
+        files = Files.new Dir.getwd + '/spec/test_dir'
+        long_format_regular = /^[-dxrw]*\s*[0-9]*\s*[a-zA-Z]*\s*[a-zA-Z]*\s*[0-9]*\s*[0-9]*\s[0-9]*\s*[0-9]*:[0-9]*\s[a-zA-Z.]*/
+        expect { files.print_long_format }.to output(long_format_regular).to_stdout
+        files.print_long_format
       end
     end
   end
