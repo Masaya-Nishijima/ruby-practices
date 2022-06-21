6d896fcf3bd7b7ce5d9458905a815fc5e89ac4c0
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-21 10:10:31 +0900

    feat: 標準入力をメモリー上で管理できるように

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,26 +6,15 @@ require 'etc'
 TEMP_FILE = '.tmp_wc'
 WIDTH = 5
 
-def main(params, path)
-  if path == TEMP_FILE
-    File.open(path, 'w+') do |tmp_file|
-      readlines.each { |line| tmp_file.write line }
-    end
-  end
-
-  File.open(path) do |file|
-    file_name = path == TEMP_FILE ? '' : path.split('/')[-1]
-    file_body = file.read
-    file_size = file.size
-    n_lines = file_body.count("\n")
-    n_lines += 1 if /[^\n]\z/.match?(file_body)
-    n_words = file_body.scan(/[!-~]+/).size
-
-    printf("%#{WIDTH}d ", n_lines)
-    printf("%#{WIDTH}d %#{WIDTH}d ", n_words, file_size, file_name) unless params[:lines]
-    printf("%s\n", file_name)
-    [n_lines, n_words, file_size]
-  end
+def count_and_print_wc_element(params, file)
+  file_size = file[:body].bytesize
+  n_lines = file[:body].count("\n")
+  n_lines += 1 if /[^\n]\z/.match?(file[:body])
+  n_words = file[:body].scan(/[!-~]+/).size
+  printf("%#{WIDTH}d ", n_lines)
+  printf("%#{WIDTH}d %#{WIDTH}d ", n_words, file_size) unless params[:lines]
+  printf("%s\n", file[:name])
+  params[:lines] ? [n_lines] : [n_lines, n_words, file_size]
 end
 
 # コマンドの引数を取得
@@ -39,9 +28,8 @@ end
 
 params = read_option
 
-has_path = ARGV[0].nil? ? [TEMP_FILE] : ARGV.each { |file| File.absolute_path(file) }
-
-total = has_path.map { |file| main(params, file) }
+has_path = ARGV[0].nil? ? [{ name: '', body: readlines.join }] : ARGV.map { |file| { name: file, body: File.open(file).read } }
+total = has_path.map { |file| count_and_print_wc_element(params, file) }
 unless has_path[1].nil?
   total.transpose.map(&:sum).each { |value| printf("%#{WIDTH}d ", value) }
   puts('total')
