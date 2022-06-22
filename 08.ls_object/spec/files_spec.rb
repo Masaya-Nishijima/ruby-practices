4fb3d041d04f38cddb67135072de752fa1633699
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-22 10:05:02 +0900

    test: スペックを整理する。

--- 08.ls_object/spec/files_spec.rb
+++ 08.ls_object/spec/files_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 RSpec.describe 'ファイル群' do
   describe 'ファイル群の表示' do
     context '基本的なファイルの表示' do
-      it 'カレントディレクトリ' do
+      it 'オプションなしで昇順表示されること' do
         files = Files.new
         expect { files.print_short_format }.to output.to_stdout
         files.print_short_format
       end
 
-      it '指定ディレクトリ' do
+      it '指定ディレクトリを昇順表示できること' do
         files = Files.new Dir.getwd + '/spec/test_dir'
         expect { files.print_short_format }.to output(/test_file10/).to_stdout
         expect { files.print_short_format }.to_not output(/\.test_file/).to_stdout
@@ -17,15 +17,15 @@ RSpec.describe 'ファイル群' do
       end
     end
     context '隠しファイルの表示' do
-      it '指定ディレクトリの隠しファイル' do
+      it '指定ディレクトリで隠しファイルを表示できること' do
         files = Files.new Dir.getwd + '/spec/test_dir', true
         expect { files.print_short_format }.to output(/\.test_file/).to_stdout
         files.print_short_format
       end
     end
 
-    context 'ファイル群の反転表示' do
-      it '指定ディレクトリの反転表示' do
+    context 'ファイル群の降順表示' do
+      it '指定ディレクトリで降順表示できること' do
         files = Files.new Dir.getwd + '/spec/test_dir'
         files.reverse!
         expect { files.print_short_format }.to output(/test_file9\s*test_file4/).to_stdout
@@ -34,7 +34,7 @@ RSpec.describe 'ファイル群' do
     end
 
     context "ファイル群をロング表示" do
-      it '指定ディレクトリをロング表示' do
+      it '指定ディレクトリをロング表示できること' do
         files = Files.new Dir.getwd + '/spec/test_dir'
         long_format_regular = /^[-dxrw]*\s*[0-9]*\s*[a-zA-Z]*\s*[a-zA-Z]*\s*[0-9]*\s*[0-9]*\s[0-9]*\s*[0-9]*:[0-9]*\s[a-zA-Z.]*/
         expect { files.print_long_format }.to output(long_format_regular).to_stdout
--- 08.ls_object/spec/main_spec.rb
+++ 08.ls_object/spec/main_spec.rb
@@ -1,57 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe 'メイン' do
-  describe '引数' do
-    context '引数を取得できること' do
-      before do
-        ARGV.clear
-      end
-      it '-a' do
-        ARGV.concat(['-a'])
-        main = Main.new
-        expect(main.params[:all]).to eq true
-        expect(main.params[:reverse]).to be_falsey
-        expect(main.params[:long_format]).to be_falsey
-      end
-
-      it '-r' do
-        ARGV.concat(['-r'])
-        main = Main.new
-        expect(main.params[:all]).to be_falsey
-        expect(main.params[:reverse]).to eq true
-        expect(main.params[:long_format]).to be_falsey
-      end
-
-      it '-l' do
-        ARGV.concat(['-l'])
-        main = Main.new
-        expect(main.params[:all]).to be_falsey
-        expect(main.params[:reverse]).to be_falsey
-        expect(main.params[:long_format]).to eq true
-      end
-
-      it '-arl' do
-        ARGV.concat(['-arl'])
-        main = Main.new
-        expect(main.params[:all]).to eq true
-        expect(main.params[:reverse]).to eq true
-        expect(main.params[:long_format]).to eq true
-      end
-    end
-  end
-
   describe "表示" do
     before do
       ARGV.clear
     end
     context "ショート表示" do
-      it "オプションなし" do
+      it "オプションなしで昇順ショート表示されること" do
         main = Main.new
         expect{main.print}.to output.to_stdout
         expect{main.print}.to_not output(/[0-9]+:[0-9]+/).to_stdout
       end
 
-      it "反転表示 指定ディレクトリ" do
+      it "指定のディレクトリで降順ショート表示されること" do
         ARGV.concat('-r spec/test_dir'.split(' '))
         main = Main.new
         expect{main.print}.to output(/test_file9\s*test_file4/).to_stdout
@@ -60,7 +21,7 @@ RSpec.describe 'メイン' do
 
     end
     context "ロング表示" do
-      it "-lのみ" do
+      it "-lを指定されたときにロング表示されること" do
         ARGV.concat('-l'.split(' '))
         main = Main.new
         expect{main.print}.to output.to_stdout
@@ -68,13 +29,44 @@ RSpec.describe 'メイン' do
       end
     end
   end
-
-
-  # it "RSpecでARGVの変更" do
-  #   ARGV.clear
-  #   ARGV.concat('-a man.rb'.split(' '))
-  #   main = Main.new
-  #   # p ARGV
-  #   expect("a").to eq "a"
-  # end
 end
+
+##### 引数処理が正しく行えているかのスペック 使用時はMainクラスの@paramsをattr_readerにすること。
+# describe '引数' do
+#   context '引数を取得できること' do
+#     before do
+#       ARGV.clear
+#     end
+#     it '-a' do
+#       ARGV.concat(['-a'])
+#       main = Main.new
+#       expect(main.params[:all]).to eq true
+#       expect(main.params[:reverse]).to be_falsey
+#       expect(main.params[:long_format]).to be_falsey
+#     end
+
+#     it '-r' do
+#       ARGV.concat(['-r'])
+#       main = Main.new
+#       expect(main.params[:all]).to be_falsey
+#       expect(main.params[:reverse]).to eq true
+#       expect(main.params[:long_format]).to be_falsey
+#     end
+
+#     it '-l' do
+#       ARGV.concat(['-l'])
+#       main = Main.new
+#       expect(main.params[:all]).to be_falsey
+#       expect(main.params[:reverse]).to be_falsey
+#       expect(main.params[:long_format]).to eq true
+#     end
+
+#     it '-arl' do
+#       ARGV.concat(['-arl'])
+#       main = Main.new
+#       expect(main.params[:all]).to eq true
+#       expect(main.params[:reverse]).to eq true
+#       expect(main.params[:long_format]).to eq true
+#     end
+#   end
+# end
